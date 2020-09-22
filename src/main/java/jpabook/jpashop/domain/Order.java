cc47e06c4438340ef7427ebe9a6a98cb139ff809
seoyun <newseoyun@gmail.com>
2020-09-22 21:30:49 +0900

    .

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -39,7 +39,7 @@ public class Order {
     //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
-        member.getOrders(). add(this);
+        member.getOrders().add(this);
     }
 
     public void addOrderItem(OrderItem orderItem) {
